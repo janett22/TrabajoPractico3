0be5b4e1bf34cf24f923b3f453119b2899f052b2
janett22 <42725888+janett22@users.noreply.github.com>
2021-04-05 21:00:33 -0300

    Initial commit

---
+++ app/src/main/java/com/example/trabajopractico3/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.trabajopractico3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private MainActivityViewModel vm;
+private EditText etDolar,etEuros,etResultdo;
+private Button btConvertir;
+private RadioButton rbDolar,rbEuros;
+private TextView tvResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etDolar = findViewById(R.id.etDolar);
+        etEuros = findViewById(R.id.etEuros);
+        tvResultado = findViewById(R.id.tvResultado);
+        btConvertir = findViewById(R.id.btConvertir);
+        rbDolar= findViewById(R.id.rbDolar);
+        rbEuros= findViewById(R.id.rbEuros);
+        vm= ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
+
+        vm.getEstadoETD().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                etDolar.setEnabled(aBoolean);
+                etDolar.setText("");
+                etEuros.setText("");
+            }
+        });
+
+        vm.getEstadoETE().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                etEuros.setEnabled(aBoolean);
+                etDolar.setText("");
+                etEuros.setText("");
+            }
+        });
+
+        vm.getConvertidor().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                tvResultado.setText(s);
+            }
+        });
+
+    }
+
+      public void convertir(View v){
+              vm.convertidor(rbDolar.isChecked(),etDolar.getText().toString(),etEuros.getText().toString());
+          }
+
+
+      public void cambiar(View v){
+          vm.cambiarEstado(v.getId(),rbDolar.getId(),rbEuros.getId());
+
+        }
+
+    }
---
+++ app/src/main/java/com/example/trabajopractico3/MainActivityViewModel.java
@@ -0,0 +1,63 @@
+package com.example.trabajopractico3;
+
+import android.app.assist.AssistStructure;
+import android.view.View;
+import android.widget.RadioButton;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+    private MutableLiveData<String> convertidor;
+    private MutableLiveData<Boolean> estadoETE;
+    private MutableLiveData<Boolean> estadoETD;
+
+
+    public LiveData<Boolean> getEstadoETE(){
+        if(estadoETE==null){
+            estadoETD=new MutableLiveData<>();
+        }
+        return estadoETE;
+    }
+
+    public LiveData<Boolean> getEstadoETD() {
+        if (estadoETD == null) {
+            estadoETD = new MutableLiveData<>();
+        }
+    return estadoETD;
+    }
+
+    public LiveData<String> getConvertidor(){
+        if(convertidor==null){
+            convertidor=new MutableLiveData<>();
+        }
+        return convertidor;
+    }
+
+    public void cambiarEstado(int idView, int rbDolar, int rbEuros){
+      if(idView==rbDolar){
+          estadoETD.setValue(true);
+          estadoETE.setValue(false);
+      }else {
+          estadoETE.setValue(true);
+          estadoETD.setValue(false);
+      }
+      }
+
+    public void convertidor(Boolean rbDolar, String etDolar, String etEuros){
+
+        if(rbDolar){
+         Double dolar = Double.parseDouble(etDolar);
+         Double resultado= (dolar*91.41);
+         convertidor.setValue(resultado+" dolares");
+        }else {
+        Double euros = Double.parseDouble(etEuros);
+        Double resultado= (euros*107.97);
+        convertidor.setValue(resultado+" euros");
+        }
+        }
+
+    }
+
+
